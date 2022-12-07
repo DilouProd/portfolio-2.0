3d8e83c4a6d80ee961e096b564f13147946f56ce
DilouProd <moreira.dylan@hotmail.fr>
2022-12-07 10:50:10 +0100

    fix image add remove homepage

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import React from 'react';
-import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
-import Portfolio from './pages/Portfolio';
-import Knowledges from './pages/Knowledges';
-import Contact from './pages/Contact';
-import NotFound from './pages/NotFound'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Portfolio from "./pages/Portfolio";
+import Knowledges from "./pages/Knowledges";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
 
 const App = () => {
   return (
@@ -14,7 +14,7 @@ const App = () => {
         <Route path="/portfolio" element={<Portfolio />} />
         <Route path="/competences" element={<Knowledges />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="*" element={<NotFound />} />
+        {/* <Route path="*" element={<NotFound />} /> */}
       </Routes>
     </>
   );
--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,60 +1,72 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Navigation = () => {
-    return (
-        <div className="sidebar">
-            <div className="id">
-                <div className="idContent">
-                    <img src="./media/profilePictureMe.jpg" alt="profile-pic" />
-                    <h3>Dylan Moreira</h3>
-                </div>
-            </div>
-
-            <div className="navigation">
-                <ul>
-                    <li>
-                    <NavLink exact="true" activeClassName="active" to="/">
-                        <i className='fas fa-home'></i>
-                        <span>Accueil</span>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink exact to="/competences">
-                        <i className='fas fa-mountain'></i>
-                        <span>Compétences</span>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink exact to="/portfolio" >
-                        <i className='fas fa-images'></i>
-                        <span>Projets</span>
-                    </NavLink>
-                    </li>
-                    <li>
-                    <NavLink exact to="/contact" >
-                        <i className='fas fa-address-book'></i>
-                        <span>Contact</span>
-                    </NavLink>
-                    </li>
-                </ul>
-            </div>
-
-            <div className="socialNetwork">
-                <ul>
-                    <li>
-                        <a href="https://www.linkedin.com/in/dylan-moreira-7624b01b6/" target="_blank" rel='noopener noreferrer'><i className='fab fa-linkedin'></i></a>
-                    </li>
-                    <li>
-                        <a href="https://github.com/DilouProd" target="_blank" rel='noopener noreferrer'><i className='fab fa-github'></i></a>
-                    </li>
-                </ul>
-                <div className="signature">
-                    <p>Dylan Moreira - 2022</p>
-                </div>
-            </div>
+  return (
+    <div className="sidebar">
+      <div className="id">
+        <div className="idContent">
+          <img src="./media/profilePictureMe.jpg" alt="profile-pic" />
+          <h3>Dylan Moreira</h3>
         </div>
-    );
+      </div>
+
+      <div className="navigation">
+        <ul>
+          <li>
+            <NavLink exact="true" activeClassName="active" to="/">
+              <i className="fas fa-home"></i>
+              <span>Accueil</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/competences">
+              <i className="fas fa-mountain"></i>
+              <span>Compétences</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/portfolio">
+              <i className="fas fa-images"></i>
+              <span>Projets</span>
+            </NavLink>
+          </li>
+          <li>
+            <NavLink exact to="/contact">
+              <i className="fas fa-address-book"></i>
+              <span>Contact</span>
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+
+      <div className="socialNetwork">
+        <ul>
+          <li>
+            <a
+              href="https://www.linkedin.com/in/dylan-moreira-7624b01b6/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://github.com/DilouProd"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <i className="fab fa-github"></i>
+            </a>
+          </li>
+        </ul>
+        <div className="signature">
+          <p>Dylan Moreira - 2022</p>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navigation;
